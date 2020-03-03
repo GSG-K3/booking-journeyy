140e349eb8e70f15b4a3bc50dcb2a587caaf9f7c
Nujood <shawar.nujood@gmail.com>
2020-03-03 15:54:06 +0200

    build xhr #10

--- public/index.js
+++ public/index.js
@@ -1,21 +1,10 @@
-window.addEventListener("load", () => {
-  
-});
+window.addEventListener("load", () => {});
+
+const signUp = () => {
+  apiCall("get", "/api/signUp", (req, res) => {
+  const     
 
 
-const signUp= ()=>{
-apiCall('get','/api/signUp',(req,res)=>{
 
-
-})
-
-}
-
-
-<script>
-  src = "./apicall.js";
-</script>
-<script>
-  src = "./index.js";
-</script>
-.
+  });
+};
