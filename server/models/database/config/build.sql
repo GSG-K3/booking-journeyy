b08f109ddfcfe934fd7a31ae84a024636513983e
rubasider <rubashka12@gmail.com>
2020-03-04 10:39:41 +0200

    add the user info to the data base #11

--- server/models/database/config/build.sql
+++ server/models/database/config/build.sql
@@ -1,5 +1,5 @@
 BEGIN;
-DROP TABLE IF EXISTS journey ,userss, uj_relation CASCADE;
+DROP TABLE IF EXISTS journey ,users, uj_relation CASCADE;
 CREATE TABLE journey (
     id SERIAL PRIMARY KEY NOT NULL,
     place_name VARCHAR( 50) NOT NULL,
@@ -8,19 +8,19 @@ CREATE TABLE journey (
     img VARCHAR( 500) NOT NULL,
     journey_des VARCHAR( 500)
 );
-CREATE TABLE userss(
+CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
     user_name VARCHAR( 50) NOT NULL,
-    phone INT ,
-    email   VARCHAR( 50),
-    user_password VARCHAR( 250)
+    phone INT NOT NULL,
+    email   VARCHAR( 50) NOT NULL,
+    user_password VARCHAR( 250) NOT NULL
 );
 CREATE TABLE uj_relation(
     id SERIAL PRIMARY KEY NOT NULL,
     journey_id INT,
     user_id int,
     FOREIGN KEY (journey_id) REFERENCES journey (id),
-    FOREIGN KEY (user_id) REFERENCES userss (id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 
--- server/models/database/queries/getdata.js
+++ server/models/database/queries/getdata.js
@@ -1,18 +1,17 @@
 const dbconnection = require('../config/connection')
 
-const getdata = (callback)=>{
-    const sql ={
-        text :`SELECT * FROM users WHERE user_name =$1 AND user_password =$2`,
-        values : [user_name ,user_password]
+const getdata = (callback) => {
+    const sql = {
+        text: `SELECT * FROM users WHERE user_name =$1 AND user_password =$2`,
+        values: [user_name, user_password]
     }
-    connection.query(sql.text,sql.values,(err,results) => {
-        if(err){
+    connection.query(sql.text, sql.values, (err, results) => {
+        if (err) {
             return callback(err);
-        }
-        else {
-            return callback(null , res.rows)
+        } else {
+            return callback(null, res.rows)
         }
     });
-} 
+}
 
 module.exports = getdata
